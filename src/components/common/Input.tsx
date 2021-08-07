16b85d1c68310b8e3289d68ed9621d5341a6c048
Jo <last2599@gmail.com>
2021-08-08 01:46:25 +0900

    최적화 : 리랜더 방지

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -86,4 +86,4 @@ const Input: React.FC<IProps> = ({
   );
 };
 
-export default Input;
+export default React.memo(Input);
--- src/components/common/Selector.tsx
+++ src/components/common/Selector.tsx
@@ -82,4 +82,4 @@ const Selector: React.FC<IProps> = ({
   );
 };
 
-export default Selector;
+export default React.memo(Selector);
