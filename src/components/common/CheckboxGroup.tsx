72ce27836153371e5e42b96f8c047381eae81c88
Jo <last2599@gmail.com>
2021-08-23 19:50:41 +0900

    CheckGroup 공용 컴포넌트 개발

---
+++ src/components/common/CheckboxGroup.tsx
@@ -0,0 +1,110 @@
+import React from "react";
+import styled from "styled-components";
+import palette from "../../styles/palette";
+
+const Container = styled.div`
+  &:after {
+    display: block;
+    content: "";
+    clear: both;
+  }
+  .checkbox-label {
+    position: relative;
+    height: 18px;
+    margin-bottom: 24px;
+    display: flex;
+    align-items: center;
+    color: ${palette.gray_48};
+    cursor: pointer;
+    float: left;
+    clear: both;
+  }
+  // IE input x버튼 삭제
+  input::-ms-clear {
+    display: none;
+  }
+  input[type="checkbox"] {
+    width: 0;
+    height: 0;
+    margin: 0;
+    border: 0;
+    -webkit-appearance: none;
+  }
+  input[type="checkbox"]:checked {
+    margin: 0;
+    border: 0;
+    -webkit-appearance: none;
+  }
+  input[type="checkbox"] + input {
+    display: none;
+  }
+  input[type="checkbox"] + span {
+    width: 18px;
+    height: 18px;
+    margin-right: 8px;
+    display: inline-block;
+    flex-shrink: 0;
+  }
+  input[type="checkbox"] + span::before {
+    content: "";
+    width: 18px;
+    height: 18px;
+    position: absolute;
+    top: 0;
+    display: inline-table;
+    border: 1px solid ${palette.gray_b0};
+    border-radius: 2px;
+    box-sizing: border-box;
+    background-color: white;
+    cursor: pointer;
+  }
+  input[type="checkbox"]:checked + span::before {
+    content: "";
+    width: 18px;
+    height: 18px;
+    display: inline-table;
+    background-color: ${palette.dark_cyan};
+    border: 0;
+    border-radius: 2px;
+    position: absolute;
+    background-image: url("/static/svg/register/dark_cyan_check_mark.svg");
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+`;
+
+interface IProps {
+  value?: string[];
+  onChange: (selected: string[]) => void;
+  options?: string[];
+}
+
+const CheckboxGroup: React.FC<IProps> = ({
+  value = [],
+  onChange,
+  options = [],
+}) => {
+  return (
+    <Container>
+      {options.map((option) => (
+        <label className="checkbox-label" key={option}>
+          <input
+            type="checkbox"
+            checked={value.includes(option)}
+            onChange={(e) => {
+              if (e.target.checked) {
+                onChange([...value, option]);
+              } else {
+                onChange(value.filter((op) => op !== option));
+              }
+            }}
+          />
+          <span />
+          {option}
+        </label>
+      ))}
+    </Container>
+  );
+};
+
+export default CheckboxGroup;
