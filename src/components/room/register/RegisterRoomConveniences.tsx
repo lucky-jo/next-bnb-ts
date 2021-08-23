54493c25f5ba8ad7f01861916e20ccece181088c
Jo <last2599@gmail.com>
2021-08-23 20:22:35 +0900

    편의시설2 체크 페이지

---
+++ src/components/room/register/RegisterRoomConveniences.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import styled from "styled-components";
+import { useDispatch } from "react-redux";
+import RegisterRoomFooter from "./RegisterRoomFooter";
+import palette from "../../../styles/palette";
+import { useSelector } from "../../../../store";
+import { registerRoomActions } from "../../../../store/registerRoom";
+import CheckboxGroup from "../../common/CheckboxGroup";
+import { convinienceList } from "../../../lib/staticData";
+
+const Container = styled.div`
+  padding: 62px 30px 100px;
+  h2 {
+    font-size: 19px;
+    font-weight: 800;
+    margin-bottom: 56px;
+  }
+  h3 {
+    font-size: 14px;
+    font-weight: bold;
+    color: ${palette.gray_76};
+    margin-bottom: 6px;
+  }
+  .register-room-step-info {
+    font-size: 14px;
+    max-width: 400px;
+    margin-bottom: 24px;
+  }
+`;
+
+const RegisterConveniences: React.FC = () => {
+  const dispatch = useDispatch();
+
+  const conveniences = useSelector((state) => state.registerRoom.conveniences);
+
+  const onChangeConviniences = (selected: string[]) => {
+    dispatch(registerRoomActions.setConveniences(selected));
+  };
+
+  return (
+    <Container>
+      <h2>게스트가 어떤 공간을 사용할 수 있나요?</h2>
+      <h3>6단계</h3>
+      <p className="register-room-step-info">
+        등록하고자 하는 숙소에서 게스트가 이용 가능한 공용 공간을 선택하세요.
+      </p>
+      <div className="register-room-conviniences-checkbox-group-wrapper">
+        <CheckboxGroup
+          value={conveniences}
+          onChange={onChangeConviniences}
+          options={convinienceList}
+        />
+      </div>
+      <RegisterRoomFooter
+        prevHref="/room/register/amentities"
+        nextHref="/room/register/photo"
+      />
+    </Container>
+  );
+};
+
+export default RegisterConveniences;
---
+++ src/pages/room/register/conveniences.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { NextPage } from "next";
+import RegisterRoomConveniences from "../../../components/room/register/RegisterRoomConveniences";
+
+const conveniences: NextPage = () => {
+  return <RegisterRoomConveniences />;
+};
+
+export default conveniences;
