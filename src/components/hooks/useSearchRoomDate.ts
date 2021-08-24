ed6467ee9bb9f492d39cf825928208cd3c63c24a
Jo <last2599@gmail.com>
2021-08-24 22:28:08 +0900

    숙소 검색 예약 날짜 인풋

---
+++ src/components/hooks/useSearchRoomDate.ts
@@ -0,0 +1,36 @@
+import { useDispatch } from "react-redux";
+import { useSelector } from "../../../store";
+import { searchRoomActions } from "../../../store/searchRoom";
+
+const useSearchRoomDate = () => {
+  const checkInDate = useSelector((state) => state.searchRoom.checkInDate);
+  const checkOutDate = useSelector((state) => state.searchRoom.checkOutDate);
+
+  const dispatch = useDispatch();
+
+  // 체크인 날짜 변경 Dispatch
+  const setCheckInDateDispatch = (date: Date | null) => {
+    if (date) {
+      dispatch(searchRoomActions.setStartDate(date.toISOString()));
+    } else {
+      dispatch(searchRoomActions.setStartDate(null));
+    }
+  };
+
+  // 체크아웃 날짜 변경 Dispatch
+  const setCheckOutDateDispatch = (date: Date | null) => {
+    if (date) {
+      dispatch(searchRoomActions.setEndDate(date.toISOString()));
+    } else {
+      dispatch(searchRoomActions.setStartDate(null));
+    }
+  };
+  return {
+    checkInDate: checkInDate ? new Date(checkInDate) : null,
+    checkOutDate: checkOutDate ? new Date(checkOutDate) : null,
+    setCheckInDateDispatch,
+    setCheckOutDateDispatch,
+  };
+};
+
+export default useSearchRoomDate;
